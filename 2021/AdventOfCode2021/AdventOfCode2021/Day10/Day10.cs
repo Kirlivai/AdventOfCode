a1e9279b4726a322261d03f878030e007619d94b
Alan <alan.sigal@epitech.eu>
2023-03-14 12:02:04 +0900

    [ADD] day 11 2021

---
+++ 2021/AdventOfCode2021/AdventOfCode2021/Day10/Day10.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    public class Day10 : IDay
+    {
+        private string[] _input;
+
+        private Dictionary<char, int> _incorrectCharacterValues = new Dictionary<char, int> {
+            { ')', 3 },
+            { ']', 57 },
+            { '}', 1197 },
+            { '>', 25137 },
+        };
+
+        private Dictionary<char, int> _incompleteCharacterValues = new Dictionary<char, int> {
+            { ')', 1 },
+            { ']', 2 },
+            { '}', 3 },
+            { '>', 4 },
+        };
+
+        public Day10()
+        {
+            _input = File.ReadAllLines(@"Inputs/Day10Input.txt");
+        }
+
+        public void RunPart1()
+        {
+            int res = 0;
+
+            foreach (var line in _input)
+            {
+                res += GetCorruptedCharacterValue(line, new Stack<char>());
+            }
+
+            Console.WriteLine($"Part 1: {res}");
+        }
+
+        
+
+        public void RunPart2()
+        {
+            List<long> scores = new();
+            
+            foreach (var line in _input)
+            {
+                Stack<char> characters = new();
+                long score = 0;
+
+                int res = GetCorruptedCharacterValue(line, characters);
+
+                if (res != 0 || characters.Count == 0) continue;
+
+                score = GetIncompleteSequenceScore(characters, score);
+
+                scores.Add(score);
+            }
+
+            scores.Sort((a, b) => b.CompareTo(a));
+
+            int middleIndex = scores.Count / 2;
+
+            Console.WriteLine($"Part 2: {scores[middleIndex]}");
+        }
+        
+        private int GetCorruptedCharacterValue(string line, Stack<char> characters)
+        {
+            foreach (var c in line)
+            {
+                if (c == '(' || c == '[' || c == '{' || c == '<')
+                {
+                    characters.Push(c);
+                }
+                else if (c == ')' || c == ']' || c == '}' || c == '>')
+                {
+                    if (characters.Count == 0)
+                        return 0;
+
+                    var last = characters.Pop();
+
+                    if (last == '(' && c != ')')
+                        return _incorrectCharacterValues[c];
+                    else if (last == '[' && c != ']')
+                        return _incorrectCharacterValues[c];
+                    else if (last == '{' && c != '}')
+                        return _incorrectCharacterValues[c];
+                    else if (last == '<' && c != '>')
+                        return _incorrectCharacterValues[c];
+                }
+            }
+            return 0;
+        }
+
+        private long GetIncompleteSequenceScore(Stack<char> characters, long score)
+        {
+            while (characters.Count > 0)
+            {
+                var c = characters.Pop();
+
+                score *= 5;
+
+                switch (c)
+                {
+                    case '(':
+                        score += _incompleteCharacterValues[')'];
+                        break;
+                    case '[':
+                        score += _incompleteCharacterValues[']'];
+                        break;
+                    case '{':
+                        score += _incompleteCharacterValues['}'];
+                        break;
+                    case '<':
+                        score += _incompleteCharacterValues['>'];
+                        break;
+                }
+            }
+            return score;
+        }
+    }
+}
---
+++ 2021/AdventOfCode2021/AdventOfCode2021/Day11/Day11.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    public class Day11 : IDay
+    {
+        private string[] _input;
+        
+        public Day11()
+        {
+            _input = File.ReadAllLines(@"Inputs/Day11Input.txt");
+        }
+
+        private List<List<int>> Parse()
+        {
+            List<List<int>> map = new();
+            
+            foreach (var line in _input)
+            {
+                List<int> mapLine = new();
+
+                foreach (var c in line)
+                {
+                    mapLine.Add(c - 48);
+                }
+
+                map.Add(mapLine);
+            }
+
+            return map;
+        }
+
+        public void RunPart1()
+        {
+            var map = Parse();
+            int steps = 100;
+            int res = 0;
+
+            for (int i = 0; i < steps; i++)
+            {
+                res += Navigate(map);
+            }
+
+            Console.WriteLine($"Part 1: {res}");
+        }
+
+        public void RunPart2()
+        {
+            var map = Parse();
+            int maxSteps = int.MaxValue;
+            int nbMapElements = map.Count * map[0].Count;
+
+            for (int i = 0; i < maxSteps; i++)
+            {
+                int res = Navigate(map);
+                
+                if (res == nbMapElements)
+                {
+                    Console.WriteLine($"Part 2: {i + 1}");
+                    return;
+                }
+            }
+            Console.WriteLine($"Part 2: {0}");
+        }
+
+        private int Navigate(List<List<int>> map)
+        {
+            HashSet<(int, int)> flashedOctopus = new();
+
+            for (int i = 0; i < map.Count; i++)
+            {
+                for (int j = 0; j < map[i].Count; j++)
+                {
+                    if (flashedOctopus.Contains((i, j))) continue;
+
+                    map[i][j] += 1;
+
+                    if (map[i][j] > 9)
+                    {
+                        flashedOctopus.Add((i, j));
+                        FlashOctopus(map, flashedOctopus, i, j);
+                    }
+                }
+            }
+
+            foreach (var (i, j) in flashedOctopus)
+            {
+                map[i][j] = 0;
+            }
+
+            return flashedOctopus.Count;
+        }
+
+        private void FlashOctopus(List<List<int>> map, HashSet<(int, int)> flashedOctopus, int i, int j)
+        {
+            for (int k = 0; k < 9; k++)
+            {
+                int x = i + k % 3 - 1;
+                int y = j + k / 3 - 1;
+
+                if (x >= 0 && x < map.Count && y >= 0 && y < map[x].Count && !flashedOctopus.Contains((x, y)))
+                {
+                    map[x][y] += 1;
+
+                    if (map[x][y] > 9)
+                    {
+                        flashedOctopus.Add((x, y));
+                        FlashOctopus(map, flashedOctopus, x, y);
+                    }
+                }
+            }
+        }
+    }
+}
